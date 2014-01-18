e450b2e05cf4ed417b204430375fb25ffa8a8ced
gabeno <gmajivu@gmail.com>
2014-01-18 09:57:15 +0300

    add model

--- app/basics.js
+++ app/basics.js
@@ -13,13 +13,69 @@ console.log(Backbone);
 
 // Models
 
-var Todo = Backbone.Model.extend();
+var Todo = Backbone.Model.extend({
+  defaults: {
+    completed: false
+  },
 
-var todo1 = new Todo();
-console.log(JSON.stringify(todo1));
+  // attrs reps what the object would be after completing the current set() or save()
+  validate: function(attrs) {
+    if (!attrs.title) {
+      return 'Please set a title!';
+    }
+  },
+
+  initialize: function() {
+    // listen to changes on all atributes on this model
+    this.on('change', function() {
+      console.log('value for this model has changed.');
+    });
+    // listen to changes on an attribute on this model
+    this.on('change:title', function() {
+      console.log('title was changed.');
+    });
+    // listen to validation errors
+    this.on('invalid', function(model, error) {
+      console.log('Model raised error: %s', error);
+      console.log(model.validationError);
+    });
+  }
+});
+
+var todo = new Todo();
+console.log(JSON.stringify(todo));
+todo.set({ completed: true }, { validate: true });
+
+var todo1 = new Todo({
+  title: "check attributes of the logged model in the console"
+});
+console.log('JSON.stringify() outputs a json string', JSON.stringify(todo1));
+console.log('model.toJSON() outputs an object',todo1.toJSON());
+console.log(todo1.get('title'));
+console.log(todo1.get('completed'));
 
 var todo2 = new Todo({
-  title: "completed setting up backbone the node way",
+  title: 'This todo is done. So take no action on this one.'
+});
+console.log(JSON.stringify(todo2));
+
+// set a single attribute
+todo2.set('title', 'Title attribute set through model.set()');
+console.log(JSON.stringify(todo2));
+
+// set multiple attributes
+todo2.set({
+  title: 'Both attributes set through model.set()',
   completed: true
 });
 console.log(JSON.stringify(todo2));
+console.log('Previous attrs: ', todo2.previousAttributes());
+console.log('Changed attrs: ', todo2.changedAttributes());
+
+console.log('underscore Methods');
+console.log('_keys: names of object properties -> ', todo2.keys().join(', '));
+console.log('_values: values of object properties -> ', todo2.values());
+console.log('_pairs: convert an object to list of key, value pairs -> ', todo2.pairs());
+console.log('_invert: swap key, value -> ', todo2.invert());
+console.log('_pick: return copy of object filtered w/ only whitelisted props -> ', todo2.pick('title'));
+console.log('_omit: return copy of object filtered to omit blacklisted props -> ', todo2.omit('title'));
