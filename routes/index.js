0176e04e78021b0bc946e1ee484e05579119a3d5
gabeno <gmajivu@gmail.com>
2014-01-20 12:58:32 +0300

    reorganize project

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  res.render('index', { title: 'UI: Backbone & Node' });
+};
+
+exports.basics = function(req, res) {
+  res.render('basics', { title: 'Backbone Basics' });
 };
