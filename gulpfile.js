57509f543fb5418581f8b30d5dc5f67280b5104c
gabeno <gmajivu@gmail.com>
2014-02-06 18:04:51 +0300

    watch test files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
+var watch = require('gulp-watch');
 
 gulp.task('test', function() {
-  return gulp.src(['tests/**/*.js'])
-    .pipe(mocha({ reporter: 'spec' }))
-    .on('error', gutil.log);
+  gulp.src(['tests/**/*.js'])
+    .pipe(watch(function(files) {
+      return files
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', gutil.log);
+    }));
 });
 
 // gulp.task('default', function() {
