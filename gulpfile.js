9802001870488c16dbbb5b85603acf2096f76b7f
gabeno <gmajivu@gmail.com>
2014-02-07 00:24:42 +0300

    lint w/ eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var watch = require('gulp-watch');
+var eslint = require('gulp-eslint');
 
 gulp.task('test', function() {
   gulp.src(['tests/**/*.js'])
     .pipe(watch(function(files) {
       return files
+        .pipe(eslint())
+        .pipe(eslint.format())
         .pipe(mocha({ reporter: 'spec' }))
         .on('error', gutil.log);
     }));
