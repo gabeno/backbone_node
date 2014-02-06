6f3e1166e53bf362b7850dff8d3206d0a705228f
gabeno <gmajivu@gmail.com>
2014-02-06 17:57:15 +0300

    add gulp-mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var mocha = require('gulp-mocha');
 
-gulp.task('default', function() {
-  // some code here
+gulp.task('test', function() {
+  return gulp.src(['tests/**/*.js'])
+    .pipe(mocha({ reporter: 'spec' }))
+    .on('error', gutil.log);
 });
+
+// gulp.task('default', function() {
+//   // some code here
+// });
