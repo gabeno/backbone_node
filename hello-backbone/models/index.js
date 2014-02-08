0de22fc6c64cc9c415cc0d4fa089b6219806b9da
gabeno <gmajivu@gmail.com>
2014-02-08 20:34:54 +0300

    add code for todo model

---
+++ hello-backbone/models/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var $ = require('jquery');
+var Backbone = require('backbone');
+var _ = require('lodash/dist/lodash.underscore');
+
+Backbone.$ = $;
+
+var Item = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      text: '',
+      done: false,
+      order: 0
+    }
+  },
+
+  initialize: function() {
+    this.set({ text: this.get('text').replace('article', 'post') }, { silent: true });
+  },
+
+  validate: function(attrs) {
+    if (attrs.hasOwnProperty('done') && !_.isBoolean(attrs.done)) {
+      return 'Todo.done must be a boolean value';
+    }
+  }
+});
+
+module.exports = Item;
